f7d7cf8d47707c64512156f7e9d27301da274571
tak-eng <shibamoto2411@gmail.com>
2021-02-28 16:57:28 +0900

    ステータスコードを返す

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,9 +1,11 @@
 class IdeasController < ApplicationController
   def index
-    @idea = Idea.includes(:tags).all.order('created_at DESC')
+    @idea = Idea.pluck(:id,:body)
+    # @idea = Idea.includes(:tags).all.order('created_at DESC')
     @tags = ActsAsTaggableOn::Tag.all
     if params[:tag]
-      @idea = Idea.tagged_with(params[:tag])
+      @idea = Idea.tagged_with(params[:tag]).pluck(:id,:body)
+      
     else
       @ideas = Idea.all
     end
@@ -16,20 +18,16 @@ class IdeasController < ApplicationController
 
   def create
     @idea = Idea.new(idea_params)
-    # if @idea.save
-    #   response_created(:idea, :create)
-    #   redirect_to root_path
-    # else    
-    #   response_unprocessable_entity(:idea)
-    #   render :new
-    # end
-  
-    if @idea.valid?
-       @idea.save
-       redirect_to root_path
-    else    
-       @idea = Idea.new
-       render :new
+
+    respond_to do |format|
+      if @idea.valid?
+        @idea.save
+        format.json { render :index, status: 201 }
+        format.html { redirect_to root_path, message: "Created" }
+      else
+        format.json { render json: @idea.errors, status: 422, message: "Unprocessable Entity" }
+        format.html { render :new }
+      end
     end
   end
 
